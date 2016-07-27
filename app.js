ccc9a7c6f47e4f8693c1c05d015dc0260361c1b4
jackhkmatthews <jackhkmatthews@gmail.com>
2016-07-27 18:31:50 +0100

    v2

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 //PROBLEM: we need a simple way to look at a users badge account and JAvaScript points rom a web browser
 //SOLUTION: Use Node.js to perform profile look ups and serve out template via HTTP
 
-//1. Create web server
-
-//Lets require/import the HTTP module
-var http = require('http');
-
 //Lets define a port we want to listen to
-var PORT=8080; 
+var PORT=8080;
 
-//Create a server
+//1. Create web server
+var http = require('http');
 var server = http.createServer(function (request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}).listen(PORT);
-console.log("Server listening on: http://localhost:%s", PORT)
-
-// , function(){
-//     //Callback triggered when server is successfully listening. Hurray!
-//     console.log("Server listening on: http://localhost:%s", PORT);
-// });
+  homeRoute(request, response);
+  }).listen(PORT);
+console.log("Server running at http://localhost:%s", PORT)
 
 //2. Handel HTTP route GET / and POST i.e. Home
-	//if url == "/...nothing..." && GET
+function homeRoute(request, response){
+  //if url == "/...nothing..." && GET
+  if (request.url === "/") {
 		//show search field
-	//if url == "/...nothing..." && POST
-		//redirect to /:username
+    response.write("Header\n");
+    response.write("Search\n");
+    response.end("Footer\n");
+  //if url == "/...nothing..." && POST
+  	//redirect to /:username
+};
+}
+
+
 
 
 //3. Handel HTTP route GET /:username i.e /chalkers
@@ -37,4 +37,4 @@ console.log("Server listening on: http://localhost:%s", PORT)
 
 //4. Function that handels the reading of files and merge in value
 	//read from file and get a string
-		//merge value into string 
+		//merge value into string
