233bfcab32e513157641eee86562d2ec612a896f
jackhkmatthews <jackhkmatthews@gmail.com>
2016-07-28 18:13:08 +0100

    v3
    USER ROUTE
    3.1 added handler for http get request with a username in URL
    3.2 compartmentalized router functions into router.js file
    3.3 linked profile.js to routers.js to app.js via "require" variables
    and "module.export"
    POPULATING USER INFORMATION
    3.4 created object to hold required profile information
    3.5 created simple response.write to print number of badges to browser
    3.6 created error response

--- app.js
+++ app.js
@@ -1,39 +1,18 @@
+var router = require("./router.js")
+
 //PROBLEM: we need a simple way to look at a users badge account and JAvaScript points rom a web browser
 //SOLUTION: Use Node.js to perform profile look ups and serve out template via HTTP
 
 //Lets define a port we want to listen to
 var PORT=8080;
 
-//1. Create web server
+// Create web server
 var http = require('http');
 var server = http.createServer(function (request, response){
-  homeRoute(request, response);
+  router.home(request, response);
+  router.user(request, response);
   }).listen(PORT);
 console.log("Server running at http://localhost:%s", PORT)
-
-//2. Handel HTTP route GET / and POST i.e. Home
-function homeRoute(request, response){
-  //if url == "/...nothing..." && GET
-  if (request.url === "/") {
-		//show search field
-    response.write("Header\n");
-    response.write("Search\n");
-    response.end("Footer\n");
-  //if url == "/...nothing..." && POST
-  	//redirect to /:username
-};
-}
-
-
-
-
-//3. Handel HTTP route GET /:username i.e /chalkers
-	//if url == "/..somehthing..."
-		//get json from Treehouse
-			//on "end"
-				//show profile
-			//on "error"
-				//show error
 
 //4. Function that handels the reading of files and merge in value
 	//read from file and get a string
